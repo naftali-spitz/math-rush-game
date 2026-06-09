45df19a91bf499168a3fabc774b392e3a02d8e89
naftali-spitz <0012ab@gmail.com>
2026-06-10 01:14:52 +0300

    Load family mode styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/app.css';
 import './styles/local-db.css';
+import './styles/family.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
